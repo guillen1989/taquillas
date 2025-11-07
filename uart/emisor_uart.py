274481286fd5f90ee006eedd8e8a637f81aeb12b
guillen1989 <107359957+guillen1989@users.noreply.github.com>
2025-11-07 17:55:40 +0100

    Add files via upload

--- uart/emisor_uart.py
+++ uart/emisor_uart.py
@@ -1,8 +1,11 @@
 from machine import UART, Pin
 import time
+from mfrc522 import MFRC522
 # Se inicializa el LED integrado de la placa ESP32
 LED_PIN = 8
 led = Pin(LED_PIN, Pin.OUT)
+# CONFIGURACIÓN MFRC522
+reader = MFRC522(spi_id=0,sck=6,miso=5,mosi=4,cs=7,rst=10)
 # Se definen las funciones para controlar el LED
 def led_on():
     led.value(1)
@@ -21,8 +24,8 @@ blink(5,100)
 # Usamos UART 2 por ser el más libre (configurable en cualquier GPIO)
 # ¡Asegúrate de que estos pines GPIO estén conectados correctamente!
 UART_ID = 1
-TX_PIN = 20  # Conectar al pin RX de la Placa B
-RX_PIN = 21  # Conectar al pin TX de la Placa B
+TX_PIN = 10  # Conectar al pin RX de la Placa B
+RX_PIN = 1  # Conectar al pin TX de la Placa B
 
 # Configuración del UART
 # baudrate debe ser el mismo en ambas placas
@@ -30,14 +33,27 @@ uart = UART(UART_ID, baudrate=115200, tx=Pin(TX_PIN), rx=Pin(RX_PIN))
 
 contador = 0
 
-print("UART Transmisor iniciado.")
+#print("UART Transmisor iniciado.")
 
 while True:
+    reader.init()
+    (stat, tag_type) = reader.request(reader.REQIDL)
     mensaje = "Hola desde ESP32 A. Mensaje #{}".format(contador)
+    if stat == reader.OK:
+        (stat, uid) = reader.SelectTagSN()
+
+        #print(type(uid))
+        tarjeta_leida=reader.tohexstring(uid)
+        #print(type(tarjeta_leida))
+        mensaje=tarjeta_leida
     # El método write() espera bytes, por lo que codificamos el string.
     # El '\n' (salto de línea) ayuda a la Placa B a saber cuándo termina el mensaje.
-    uart.write(mensaje.encode('utf-8') + b'\n')
-    print("Mensaje enviado:", mensaje)
-    contador += 1
-    blink(1,50)
-    time.sleep(2)
+        #print(len(mensaje))
+        if len(mensaje)==24:
+            #print("leído correcto, longitud 24")
+            #print(mensaje)
+            uart.write(mensaje.encode('utf-8') + b'\n')
+            #print("Mensaje enviado:", mensaje)
+        contador += 1
+        blink(1,50)
+        time.sleep(2)
